15042cb459d8ac779110461b2d4c8f126def07c8
kpswamy540@gmail.com <kpswamy540@gmail.com>
2019-04-19 23:25:04 +0530

    Added category list

--- src/app/category/list/list.component.ts
+++ src/app/category/list/list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit,ViewChild } from '@angular/core';
 import {RestApiService} from '../../service/rest-api.service';
-import {MatSort, MatTableDataSource} from '@angular/material';
+import {MatSort, MatTableDataSource,MatPaginator} from '@angular/material';
 import { Category } from '../category';
 @Component({
   selector: 'app-list',
@@ -10,8 +10,9 @@ import { Category } from '../category';
 export class ListComponent implements OnInit {
   categories:Category[];
   sortedData:Category[];
-  displayedColumns=['id','name','code','isActive','createdAt','updatedAt'];
+  displayedColumns=['id','name','code','isActive'];
   dataSource = new MatTableDataSource(this.categories);
+  @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
   constructor(private restApiService:RestApiService) {
     this.getAll();
@@ -19,6 +20,7 @@ export class ListComponent implements OnInit {
    }
   
   ngOnInit() {
+    this.dataSource.paginator=this.paginator;
     this.dataSource.sort = this.sort;
   }
   
@@ -34,6 +36,14 @@ export class ListComponent implements OnInit {
       }    
     })
   }
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // Datasource defaults to lowercase matches
+    this.dataSource.filter = filterValue;
+    if (this.dataSource.paginator) {
+        this.dataSource.paginator.firstPage();
+    }
+}
 
 }
 
