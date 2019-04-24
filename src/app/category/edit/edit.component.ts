580483957d713cc296e289e42ca2d29ee7dab4e0
kpswamy540@gmail.com <kpswamy540@gmail.com>
2019-04-24 07:41:16 +0530

    Edit

--- src/app/category/edit/edit.component.ts
+++ src/app/category/edit/edit.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Category } from '../category';
 import { RestApiService } from 'src/app/service/rest-api.service';
 import {ActivatedRoute  } from "@angular/router";
+import {FormGroup,FormBuilder,Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-edit',
@@ -13,11 +14,20 @@ export class EditComponent implements OnInit {
   selectedId: number;
   errorMessage: string;
   hasError: Boolean;
-  constructor(private restApiService: RestApiService,private router: ActivatedRoute) { }
+  editForm: FormGroup;
+  showSpinner: Boolean;
+  isOpCompleted: Boolean;
+  constructor(private restApiService: RestApiService,private router: ActivatedRoute,private fb: FormBuilder) { }
 
   ngOnInit() {
     this.selectedId=this.router.snapshot.params['id'];
     this.getCatgory(this.selectedId);
+    this.editForm=this.fb.group({
+			'name':[null,Validators.required],
+			'code':[null,Validators.required],
+      'isActive':[null]
+
+		});
     
   }
   getCatgory(id){
@@ -25,12 +35,20 @@ export class EditComponent implements OnInit {
     this.restApiService.getAll(url).subscribe(result=>{
       if(result.status==200){
         this.category=result.category;
+        this.editForm.setValue({
+          'name':this.category.name,
+          'code':this.category.code,
+          'isActive':this.category.isActive
+        })
       }
       else{
         this.errorMessage="No category is exists with provided input."
         this.hasError=true;
       }
     })
+  }
+  edit(id){
+    console.log(this.editForm.value);
   }
 
 }
