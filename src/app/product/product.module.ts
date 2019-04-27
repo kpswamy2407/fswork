464822dd2a7244cf9cbdaec0251e31d2e6d40823
kpswamy540@gmail.com <kpswamy540@gmail.com>
2019-04-27 22:23:18 +0530

    Added product module

---
+++ src/app/product/product.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProductRoutingModule } from './product-routing.module';
+import { AddComponent } from './add/add.component';
+import { EditComponent } from './edit/edit.component';
+import { ListComponent } from './list/list.component';
+
+@NgModule({
+  declarations: [AddComponent, EditComponent, ListComponent],
+  imports: [
+    CommonModule,
+    ProductRoutingModule
+  ]
+})
+export class ProductModule { }
