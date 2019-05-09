e0a9789221d7633c91d5760c69a885ca6a0a2716
MSS <kpswamy540@gmail.com>
2019-05-09 15:07:37 +0530

    Added dashboard

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatButtonModule, MatCardModule, MatIconModule, MatTableModule } from '@angular/material';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
@@ -8,7 +11,12 @@ import { DashboardComponent } from './dashboard.component';
   declarations: [DashboardComponent],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    FlexLayoutModule.withConfig({addFlexToParent: false}),
+    MatTableModule,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
   ]
 })
 export class DashboardModule { }
