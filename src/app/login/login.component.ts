642b74e4466b465a21af254072423b3cca351e47
MSS <kpswamy540@gmail.com>
2019-05-13 16:46:58 +0530

    login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,6 +5,7 @@ import {
 	FormBuilder,
 	Validators
 } from '@angular/forms';
+import { BehaviorSubject, Observable } from 'rxjs';
 import {RestApiService} from '../service/rest-api.service';
 
 @Component({
@@ -17,7 +18,13 @@ export class LoginComponent implements OnInit {
     errorMessage: String;
     hasError: Boolean;
     showSpinner: Boolean;
+    private currentUserSubject: BehaviorSubject<any>;
+    public currentUser: Observable<any>;
+
     constructor(private router: Router,private restApiService: RestApiService, private fb: FormBuilder) {
+        this.currentUserSubject = new BehaviorSubject<any>(JSON.parse(localStorage.getItem('currentUser')));
+        this.currentUser = this.currentUserSubject.asObservable();
+
     	this.logiForm=this.fb.group({
     		'username':[null,Validators.required],
     		'password':[null,Validators.required]
@@ -27,18 +34,19 @@ export class LoginComponent implements OnInit {
     ngOnInit() {}
 
     onLogin() {
-    	console.log(this.logiForm.value)
     	if(this.logiForm.valid){
     		this.showSpinner=true;
         	this.restApiService.postData('users/login',this.logiForm.value).subscribe(result=>{
-                console.log(result);
+                if(result.status==200){
+                    this.router.navigate(['/dashboard']);
+                }
+                else{
+                    this.errorMessage=result.error;
+                    this.showSpinner=false;
+                }
             })
-            this.router.navigate(['/dashboard']);
+            
     	}
-    	else{
-    		console.log(this.logiForm.value)
-    	}
-        
     }
 }
 
