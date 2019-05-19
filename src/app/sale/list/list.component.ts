c2d0edaa6e9642d0b9020d31a3c793ccab04a71c
kpswamy540@gmail.com <kpswamy540@gmail.com>
2019-05-19 08:38:29 +0530

    sale list

--- src/app/sale/list/list.component.ts
+++ src/app/sale/list/list.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit,ViewChild } from '@angular/core';
+import { Sale} from '../sale';
+import {RestApiService} from '../../service/rest-api.service';
+import {DialogService} from '../../service/dialog.service';
+import {MatTableDataSource,MatDialog,MatSort,MatPaginator} from '@angular/material';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
@@ -7,9 +11,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
-
+  sales:Sale[];
+  sortedData:Sale[];
+  displayedColumns=['id','customer','totalAmount','createdAt','action'];
+  dataSource = new MatTableDataSource(this.sales);
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  constructor(private restApiService:RestApiService,private dialogService:DialogService,private dialog: MatDialog,private router: Router) {
+    this.getAll();
+    
+   }
+  
   ngOnInit() {
+    this.dataSource.paginator=this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+  
+
+  getAll(){
+    this.restApiService.getAll('sale/getall').subscribe(result=>{
+      if(result.status==200){
+        this.sales=result.sales;
+        this.dataSource.data=this.sales;
+      }
+      else{
+        this.sales=[];
+      }    
+    })
+  }
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // Datasource defaults to lowercase matches
+    this.dataSource.filter = filterValue;
+    if (this.dataSource.paginator) {
+        this.dataSource.paginator.firstPage();
+    }
+  }
+  onDelete(id){
+    this.dialogService.openConfirmDialog('Are you sure to delete this record ?')
+    .afterClosed().subscribe(res =>{
+      if(res){
+        this.restApiService.deleteData('product/delete',id).subscribe(res=>{
+          if(res.status==200){
+            this.getAll();
+          }
+        })
+      }
+    });
+  }
+  edit(id){
+    this.router.navigate(['product/edit',id]);
   }
 
 }
--- src/app/sale/sale.ts
+++ src/app/sale/sale.ts
@@ -1,2 +1,7 @@
 export class Sale {
+	id:number
+	customerId: number;
+	totalAmount: number;
+	createdAt:Date
+    updatedAt:Date
 }
