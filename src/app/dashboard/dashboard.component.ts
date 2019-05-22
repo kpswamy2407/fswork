cfe004ca8537d7b4082f0da931798e7842c3e894
kpswamy540@gmail.com <kpswamy540@gmail.com>
2019-05-22 08:35:27 +0530

    dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,22 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material';
 import {RestApiService} from '../service/rest-api.service';
-export interface PeriodicElement {
-    name: string;
-    position: number;
-    weight: number;
-    symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-    { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-    { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-    { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-    { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-    { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-    { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-    { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' }
-];
+import {Sale} from '../sale/sale';
 
 @Component({
     selector: 'app-dashboard',
@@ -24,8 +9,10 @@ const ELEMENT_DATA: PeriodicElement[] = [
     styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-    displayedColumns = ['position', 'name', 'weight', 'symbol'];
-    dataSource = new MatTableDataSource(ELEMENT_DATA);
+    sales: Sale[];
+
+    displayedColumns=['id','customer','totalAmount','createdAt','action'];
+    dataSource = new MatTableDataSource(this.sales);
     places: Array<any> = [];
     records: any;
     applyFilter(filterValue: string) {
@@ -36,43 +23,22 @@ export class DashboardComponent implements OnInit {
 
     constructor(private restApiService: RestApiService) {
         this.getDashboard();
-        this.places = [
-            {
-                imgSrc: 'assets/images/card-1.jpg',
-                place: 'Cozy 5 Stars Apartment',
-                description:
-                    // tslint:disable-next-line:max-line-length
-                    'The place is close to Barceloneta Beach and bus stop just 2 min by walk and near to "Naviglio" where you can enjoy the main night life in Barcelona.',
-                charge: '$899/night',
-                location: 'Barcelona, Spain'
-            },
-            {
-                imgSrc: 'assets/images/card-2.jpg',
-                place: 'Office Studio',
-                description:
-                    // tslint:disable-next-line:max-line-length
-                    'The place is close to Metro Station and bus stop just 2 min by walk and near to "Naviglio" where you can enjoy the night life in London, UK.',
-                charge: '$1,119/night',
-                location: 'London, UK'
-            },
-            {
-                imgSrc: 'assets/images/card-3.jpg',
-                place: 'Beautiful Castle',
-                description:
-                    // tslint:disable-next-line:max-line-length
-                    'The place is close to Metro Station and bus stop just 2 min by walk and near to "Naviglio" where you can enjoy the main night life in Milan.',
-                charge: '$459/night',
-                location: 'Milan, Italy'
-            }
-        ];
+        this.getRecentSale();
     }
 
     ngOnInit() {}
     getDashboard(){
         this.restApiService.getAll('users/dashboard').subscribe(result=>{
-            console.log(result);
-            if(result.status=200){
+             if(result.status=200){
                 this.records=result.records;
+            }
+        })
+    }
+     getRecentSale(){
+        this.restApiService.getAll('sale/recent').subscribe(result=>{
+            if(result.status=200){
+                this.sales=result.sales;
+                this.dataSource.data=this.sales;
             }
         })
     }
